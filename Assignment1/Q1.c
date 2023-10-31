06248567d1ffd29c3e00459961ec50208d512354
Moon Shadow <moon.shadow.rev@gmail.com>
2023-10-31 12:07:26 +0300

    [Add]: Question 1 Solution

--- Assignment1/Q1.c
+++ Assignment1/Q1.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+// Question 1: Find the greatest number among three numbers entered by the user from the keyboard.
 int main(){
+    // a, b, c are three numbers
+    int a, b, c;
+    // Print to user that we need inputs
+    printf("%s", "Enter three numbers: \n");
+    // Take inputs from user
+    scanf("%d %d %d", &a, &b, &c);
+    // Checks for Greatest number
+    if ( a > b ) {
+        if ( a > c ) {
+            printf("%d %s", a," is the greatest number \n");
+        } else {
+            printf("%d %s", c, " is the greatest number \n");
+        }
+    } else {
+        if ( b > c ) {
+            printf("%d %s", b, " is the greatest number \n");
+        } else {
+            printf("%d %s", c, " is the greatest number \n");
+        }
+    }
     return 0;
 }
+
 
